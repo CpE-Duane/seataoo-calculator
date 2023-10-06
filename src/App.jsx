146d456774a633fed931ff0df4ef493d6aebe430
CpE-Duane <villapando.duane@gmail.com>
2023-10-06 21:45:28 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Header from './components/Header'
 import FileUpload from './components/FileUpload'
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
      return (
           <Routes>
-               <Route path='' element={<FileUpload />} />
+               <Route path='/' element={<FileUpload />} />
           </Routes>
      )
 }
